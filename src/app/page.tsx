98b035164dea0e3be37043cb6df1393a34ac5c37
Nhat Dang <nhatdzfbk@gmail.com>
2025-06-22 00:31:19 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div>
             <div className="h-[569px] md:h-[598px] w-full relative flex justify-center items-center">
                 <Image src="/banner.png" alt="banner" fill className="object-cover" />
-                <p className={`text-white text-center z-50 text-2xl md:text-5xl leading-9 md:leading-17 uppercase ${trajanPro3.className}`}>
+                <p className={`text-white text-center z-10 text-2xl md:text-5xl leading-9 md:leading-17 uppercase ${trajanPro3.className}`}>
                     Amor Thảo Mộc <br /> Nơi Lưu Giữ Bình Yên
                 </p>
             </div>
