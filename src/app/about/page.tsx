4a7acafc85c1d25a05636b273d8e31b21f002e4c
Nhat Dang <nhatdzfbk@gmail.com>
2025-06-22 00:33:07 +0700

    fix z-index

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,7 +13,7 @@ const About = () => {
             <div className="w-full relative flex flex-col justify-center items-center pt-[80px]">
                 <Image src="/banner-about.png" alt="banner" width={399} height={556} className="object-cover hidden md:block" />
                 <Image src="/banner-about-mobile.png" alt="banner" width={226} height={317} className="object-cover block md:hidden" />
-                <p className={`mt-10 text-[#824435] text-center z-50 text-2xl font-semibold md:text-[40px] leading-9 md:leading-[56px] uppercase ${trajanPro3.className}`}>
+                <p className={`mt-10 text-[#824435] text-center z-10 text-2xl font-semibold md:text-[40px] leading-9 md:leading-[56px] uppercase ${trajanPro3.className}`}>
                     Mến chào quý khách,<br />
                     Tôi là Ty Anh Nguyễn
                 </p>
